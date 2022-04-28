282534268a5375bf3de2bdf6444780fe4d3d0daf
Aman <amsingh714@gmail.com>
2022-04-28 15:46:24 -0400

    page is mobile responsive in all sizes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 document.addEventListener("click", (e) => {
   const isDropdownLink = e.target.matches("[data-dropdown-link]");
   if (!isDropdownLink && e.target.closest("[data-dropdown]") != null) return;
-  console.log(isDropdownLink);
 
   let currentDropdown;
   if (isDropdownLink) {
     currentDropdown = e.target.closest("[data-dropdown]");
-    console.log(currentDropdown);
+
     currentDropdown.classList.toggle("active");
   }
 
@@ -14,7 +13,6 @@ document.addEventListener("click", (e) => {
     if (dropdown === currentDropdown) return;
     dropdown.classList.remove("active");
   });
-  console.log(currentDropdown.classList);
 });
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
